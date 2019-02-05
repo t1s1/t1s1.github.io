08b5f09ddc457deff57694f49f792aa1cfd25e0d
Ted Singdahlsen <Ted.Singdahlsen@globalknowledge.com>
2019-02-05 14:29:27 -0500

    added course code back more data fixes

--- js/coursedata.js
+++ js/coursedata.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
     $('#datatable').DataTable( {
         ajax: "../data/courselist.txt",
         columns: [
+            {"data": "GK Code"},
             {"data": "Source"},
             {"data": "Course Name"},
             {"data": "Product Line"},
